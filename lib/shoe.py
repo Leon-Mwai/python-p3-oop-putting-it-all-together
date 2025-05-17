8b555660aefef28fc624afebd1f830193edc9409
leon mwai <leon.mwai@student.moringaschool.com>
2025-05-17 11:47:23 +0300

    added book.py and shoe.py

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,36 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = None  # Optional attribute to be set later
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        self._brand = value
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, (int, float)):
+            self._size = value
+        else:
+            print("size must be a number")
+
+    @property
+    def condition(self):
+        return self._condition
+
+    @condition.setter
+    def condition(self, value):
+        self._condition = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
